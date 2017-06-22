023de76439b0d0e0c259452ec57edccc78ed5ffd
Macbit <antonpuzorjov@antons-mbp.home>
2017-06-22 07:28:34 +0100

    Qiu 2007, attempt to reproduce Fig 8.
    p. 440

--- Qiu_2007_PyDDE.py
+++ Qiu_2007_PyDDE.py
@@ -27,17 +27,17 @@ import matplotlib.pyplot as plt
 # P0 = 1.0e6 # particles/ml starting levels of cells (Levin et al., 1977)
 
 # # Setting initial values (as in Levin 1977)
-u = 10.0 #0.738 # as in Krysiak-Baltyn, 2016 vs 0.738(Levin et al., 1977)
+u = 0.738 # as in Krysiak-Baltyn, 2016 vs 0.738(Levin et al., 1977)
 S0 = 100.0 #ug/ml(Levin et al., 1977)
-D = 9.0 #0.20 # h-1  #(Levin et al., 1977)
+D = 0.20 # h-1  #(Levin et al., 1977)
 Ki = 6.24e-8 #ml/h (Levin et al., 1977)
 b = 100.0 #(Levin et al., 1977)
-Km = 2.0#4.0 #4 ug/ml(Levin et al., 1977)
-Y = 0.3#7.40e4 #(Levin et al., 1977)
-q = 0.7# 0.35 # induction rate (...)
-T = 0 # ODL
-Xs0 = 3.789e7 #1.0e4 # cells/ml starting levels of cells (Levin et al., 1977)
-P0 = 1.0e6 # particles/ml starting levels of cells (Levin et al., 1977)
+Km = 4.0 #4 ug/ml(Levin et al., 1977)
+Y = 10e6/2.6#7.40e4 #(Levin et al., 1977)
+q = 0.35 # induction rate (...)
+T = 0.0 # ODE
+Xs0 = 10.0#3.789e7 #1.0e4 # cells/ml starting levels of cells (Levin et al., 1977)
+P0 = 10.0#1.0e6 # particles/ml starting levels of cells (Levin et al., 1977)
 
 dde_camp = p.dde()
 
@@ -83,7 +83,7 @@ ddestsc = array([0,0,0,0])
 #           tol=0.000005, dt=1.0, hbsize=10000, nlag=1, ssc=ddestsc)
 
 # Long version
-dde_camp.initproblem(no_vars=4, no_cons=9, nlag=1, nsw=0, t0=0.0, t1=250.0, initstate=ddeist, c=ddecons, otimes= arange(0.0, 250.0, 0.1), grad=ddegrad, storehistory=ddesthist)
+dde_camp.initproblem(no_vars=4, no_cons=9, nlag=1, nsw=0, t0=0.0, t1=100.0, initstate=ddeist, c=ddecons, otimes= arange(0.0, 100.0, 0.1), grad=ddegrad, storehistory=ddesthist)
 
 dde_camp.initsolver(tol=0.000005, hbsize=1000, dt=1.0, statescale=ddestsc)
 
@@ -95,12 +95,11 @@ print(dde_camp.data)
 plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 2],  label=r'Xs')
 #plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 3],  label=r'Xl')
 plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 4],  label=r'P')
-#plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 1],  label=r'S')
 plt.legend()
 plt.xlabel('Time (hours)')
 plt.ylabel('Log concentration (particles/ml)')
 plt.yscale('log')
-plt.axis([-5,250,-100,10000000000])
+plt.axis([0,100,-100,10000000000])
 plt.tick_params(
     axis='both', # changes apply to both axis
     labelsize=12) # set new font size
