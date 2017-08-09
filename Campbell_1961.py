7c3d5ec9e1285342371730a18d5393ad4c0f8808
Macbit <puzerjov@gmail.com>
2017-08-09 07:44:50 +0100

    Campbell is revised to common standard

--- Campbell_1961.py
+++ Campbell_1961.py
@@ -11,9 +11,11 @@ D = 0.20 # h-1  #(Levin et al., 1977)
 Ki = 6.24e-8 #ml/h (Levin et al., 1977)
 b = 98.0 # (Levin et al., 1977)
 dp = 0.1 # 0... 0.80 h-1 (in full sunlight) (Suttle & Chen, 1992)
-T = 0.5 # h-1 (Levin et al., 1977)
+T = 0. # h-1 (Levin et al., 1977)
 Xs0 = 1.0e4 # cells/ml starting leveles of cells (Levin et al., 1977)
 P0 = 1.0e6 # particles/ml starting leveles of cells (Levin et al., 1977)
+
+sim_length = 250.0 # set the simulation length time
 
 dde_camp = p.dde()
 
@@ -49,29 +51,24 @@ ddeist = array([Xs0, P0])
 # Setting a state-scaling array for use in error control when values are very close to 0
 ddestsc = array([0,0])
 
-# Short version (not used)
-#dde_camp.dde(y=ddeist, times=arange(0.0, 250.0, 1.0),
-#           func=ddegrad, parms=ddecons,
-#           tol=0.000005, dt=1.0, hbsize=10000, nlag=1, ssc=ddestsc)
-
 # Long version
-dde_camp.initproblem(no_vars=2, no_cons=7, nlag=1, nsw=0, t0=0.0, t1=250.0, initstate=ddeist, c=ddecons, otimes= arange(0.0, 250.0, 0.1), grad=ddegrad, storehistory=ddesthist)
-
+dde_camp.initproblem(no_vars=2, no_cons=7, nlag=1, nsw=0, t0=0.0, t1=sim_length, initstate=ddeist, c=ddecons, otimes= arange(0.0, sim_length, 0.1), grad=ddegrad, storehistory=ddesthist)
 dde_camp.initsolver(tol=0.000005, hbsize=10000, dt=1.0, statescale=ddestsc)
-
 dde_camp.solve()
 
-print(dde_camp.data)
+# Plot figures
+plt.style.use('ggplot') # set the global style
+p, = plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 2], ":",  label=r'$P$')
+xs, = plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 1],  label=r'$X_S$')
 
-plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 1],  label=r'$X_S$')
-plt.plot(dde_camp.data[:, 0], dde_camp.data[:, 2], "r:",  label=r'$P$')
-plt.legend()
-plt.xlabel('Time (hours)')
-plt.ylabel('Log concentration (particles/ml)')
+f_size = 15 # set font size for plot labels
+plt.legend(prop={'size': f_size})
+plt.xlabel('Time (hours)', fontsize=f_size)
+plt.ylabel('Log concentration (particles/ml)', fontsize=f_size)
 plt.yscale('log')
-plt.axis([-5,250,-100,10000000000])
-plt.tick_params(
-    axis='both', # changes apply to both axis
-    labelsize=12) # set new font size
+plt.axis([0,sim_length,1.0e-4,1.0e10])
+#plt.text(sim_length*0.8,2.0e9,'$S$= '+str(S0)) # display parameters
+plt.tick_params(axis='both', labelsize=f_size)
 plt.tight_layout()
-plt.show()
+#plt.show()
+plt.savefig('Campbell_PyDDE.pdf')
